bb4efe8e4f08fda9f5e326afe39f21be17f1375f
Nguyen Louis <nguyenlouis172@gmail.com>
2025-02-06 18:54:22 +0100

    update getCapacityFuelCar, consume, calculatePrice

--- Car.java
+++ Car.java
@@ -11,18 +11,15 @@ public class Car {
         this.numberRegisterCar = numberRegisterCar;
         this.speed = speed;
         this.numberKm = numberKm;
-        checkQuantityFuelCar();
-    }
-
-    public void checkQuantityFuelCar() {
-        if (power < 7) {
-            capacityFuelCar = 40;
-        } else {
-            capacityFuelCar = 60;
-        }
+        getCapacityFuelCar();
     }
 
     public float getCapacityFuelCar() {
+        if (power >= 4 && power <= 6) {
+            capacityFuelCar = 40;
+        } else if (power >= 7 && power <= 200) {
+            capacityFuelCar = 60;
+        }
         return capacityFuelCar;
     }
 
@@ -48,13 +45,13 @@ public class Car {
     }
 
     public float consume() {
-        if (speed <= 80) {
+        if (speed > 1 && speed <= 80) {
             consumption = 0.06f;
-        } else if (speed <= 100) {
+        } else if (speed > 80 && speed <= 100) {
             consumption = 0.07f;
-        } else if (speed <= 120) {
+        } else if (speed > 100 && speed <= 120) {
             consumption = 0.08f;
-        } else if (speed <= 130) {
+        } else if (speed > 120 && speed <= 130) {
             consumption = 0.09f;
         } else {
             // Valeur par défaut si la vitesse est hors de la plage spécifiée
@@ -67,7 +64,7 @@ public class Car {
 
     public float calculatePrice() {
         int breakdowns = isBreakdown();
-        price = totalFuelUsed * FUEL_PRICE + breakdowns * 100;
+        price = totalFuelUsed * FUEL_PRICE + (breakdowns * 100) + (breakdowns * capacityFuelCar);
         return price;
     }
 }
